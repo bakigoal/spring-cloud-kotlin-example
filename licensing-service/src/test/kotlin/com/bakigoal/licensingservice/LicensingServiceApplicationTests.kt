82fc6c41a02e62011da333832719e9477cab530e
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-02-14 18:28:10 +0500

    added license service skeleton

---
+++ licensing-service/src/test/kotlin/com/bakigoal/licensingservice/LicensingServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bakigoal.licensingservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
