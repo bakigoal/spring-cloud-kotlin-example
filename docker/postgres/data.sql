5c29daa45a0629af0ad33ab0bd26ef67d13a8c64
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-02-15 16:31:47 +0500

    update docker-compose.yml

---
+++ docker/postgres/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.organizations VALUES ('e6a625cc-718b-48c2-ac76-1dfdff9a531e', 'Organization 1', 'John Doe', 'john@gmail.com', '888888888');
+INSERT INTO public.organizations VALUES ('d898a142-de44-466c-8c88-9ceb2c2429d3', 'Organization 2', 'Admin', 'admin@gmail.com', '888888888');
+INSERT INTO public.organizations VALUES ('e839ee96-28de-4f67-bb79-870ca89743a0', 'Organization 3', 'James Salt', 'james@gmail.com', '888888888');
+INSERT INTO public.licenses VALUES ('f2a9c9d4-d2c0-44fa-97fe-724d77173c62', 'd898a142-de44-466c-8c88-9ceb2c2429d3', 'Software Product', 'Product 1', 'complete', 'I AM DEV');
+INSERT INTO public.licenses VALUES ('279709ff-e6d5-4a54-8b55-a5c37542025b', 'e839ee96-28de-4f67-bb79-870ca89743a0', 'Software Product', 'Product 2', 'complete', 'I AM DEV');
