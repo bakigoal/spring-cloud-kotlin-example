94af8192f166a9a5d367ef11140c00c4a9a9e6b7
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-02-17 16:06:58 +0500

    refactoring

--- restart-service.sh
+++ restart-service.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-function rebuild_service {
-  service=$1
-  echo restarting "$service"
-  app=spring-cloud-kotlin-example
-
-  docker stop "$app-$service-1"
-  docker image rm -f $app"_$service"
-}
-
 services=("${@:1}")
+app="spring-cloud-kotlin-example"
+
 for service in "${services[@]}"
 do
-  rebuild_service "$service"
+  docker_container="$app-$service-1"
+  docker_image=$app"_$service"
+
+  echo "stopping container $docker_container"
+  docker stop "$docker_container"
+  echo
+  echo "removing image $docker_image"
+  docker image rm -f "$docker_image"
+  echo
 done
 
+echo "building services"
 ./gradlew clean bootJar
+echo
+
+echo "restarting docker-compose"
 docker-compose up -d
